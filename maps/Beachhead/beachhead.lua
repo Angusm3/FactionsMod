702ecbcf1db95a24457fd6677cbbd96fb8daa8db
Angusm3 <treeforge.al@gmail.com>
2014-09-26 15:17:39 -0700

    lots of changes
    give me more coffee

--- maps/Beachhead/beachhead.lua
+++ maps/Beachhead/beachhead.lua
@@ -1,13 +1,16 @@
 BaitReinforcements1 = { "e1", "e1", "e1" }
 VehicleReinforcements = { "jeep" }
 InfantryReinforcements = { "e2", "e2", "e1" }
-TanyaType = { "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN", "TSARIFLEMAN" }
-Rockets = { "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSAROCKET", "TSABURSTSNIPER", "TSAMECH", "TSAMECH" }
-TSAV = { "TSAHMV", "TSAHMV", "TSAHMV", "TSAHMV", "TSAHMV", "TSAHMV", "MEDTANK", "MEDTANK", "MEDTANK", "MEDTANK", "MEDTANK", "MAMMOTH2", "OSIRIS", "OSIRIS" }
-MARINF1 = { "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE", "MARRIFLE" }
-MARINF2 = { "MARFLAMMENWERFER", "MARFLAMMENWERFER", "MARFLAMMENWERFER", "MARPANZERSHREK", "MARPANZERSHREK", "MARPANZERSHREK", "MARPANZERSHREK" }
-BAITMG = { "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG", "BAITMG" }
-BAITINF = { "BAITSMAW", "BAITSMAW", "BAITSMAW", "BAITSMAW", "BAITSMAW", "BAITSNIPER", "BAITFLAMETHROWER", "BAITFLAMETHROWER", "BAITMEDIC", "BAITMORTAR", "BAITMORTAR", "BAITMORTAR" }
+
+
+--FactionAssignment = function()
+--		INF1 = MARINF1
+--		INF2 = MARINF2
+--		V = MARV
+--end
+
+
+
 
 
 InsertionHelicopterType = "tran"
@@ -17,14 +20,17 @@ WorldLoaded = function()
 	player = OpenRA.GetPlayer("Marzeron")
 	player2 = OpenRA.GetPlayer("Baition")
 	player3 = OpenRA.GetPlayer("TSA")
-
 	
-	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea1(BAITMG) end)
-	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea2(BAITMG) end)
-	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea3(BAITMG) end)
-	OpenRA.RunAfterDelay(50 * 5, function() ReinforceFromSea4(BAITMG) end)
-	OpenRA.RunAfterDelay(50 * 5, function() ReinforceFromSea5(BAITINF) end)
-	OpenRA.RunAfterDelay(50 * 5, function() ReinforceFromSea6(BAITV) end)
+	p1 = 6
+	p2 = 1
+	p3 = 13
+	
+	
+	
+	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea1(TEAM2INF1) end)
+	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea2(TEAM2INF2) end)
+	OpenRA.RunAfterDelay(1 * 5, function() ReinforceFromSea3(TEAM2V) end)
+
 
 	SendMarReinforcements()
 	SendInsertionHelicopter()
@@ -34,14 +40,15 @@ end
 
 
 SendMarReinforcements = function()
+	FactionAssignment()
 
-	Reinforcements.Reinforce(player, MARINF1, MarInsert.Location, MarRally1.Location, Utils.Seconds(0.2), function(actor)
+	Reinforcements.Reinforce(player, TEAM1INF1, MarInsert.Location, MarRally1.Location, Utils.Seconds(0.2), function(actor)
 		Actor.SetStance(actor, "Defend")
 	end)
-	Reinforcements.Reinforce(player, MARINF2, MarInsert.Location, MarRally2.Location, Utils.Seconds(0.2), function(actor)
+	Reinforcements.Reinforce(player, TEAM1INF2, MarInsert.Location, MarRally2.Location, Utils.Seconds(0.2), function(actor)
 		Actor.SetStance(actor, "Defend")
 	end)
-	Reinforcements.Reinforce(player, MARV, MarInsert.Location, MarRally2.Location, Utils.Seconds(1), function(actor)
+	Reinforcements.Reinforce(player, TEAM1V, MarInsert.Location, MarRally2.Location, Utils.Seconds(1), function(actor)
 		Actor.SetStance(actor, "Defend")
 	end)
 
@@ -62,41 +69,31 @@ ReinforceFromSea3 = function(passengers)
 	local hovercraft, troops = Reinforcements.Insert(player2, "LST", passengers, { lcfentry3.Location, lcfdropoff3.Location  }, { lcfentry3.Location })
 end
 
-ReinforceFromSea4 = function(passengers)
-	local hovercraft, troops = Reinforcements.Insert(player2, "LST", passengers, { lcfentry1.Location, lcfdropoff1.Location  }, { lcfentry1.Location })
-end
 
-ReinforceFromSea5 = function(passengers)
-	local hovercraft, troops = Reinforcements.Insert(player2, "LST", passengers, { lcfentry2.Location, lcfdropoff2.Location  }, { lcfentry2.Location })
-end
-
-ReinforceFromSea6 = function(passengers)
-	local hovercraft, troops = Reinforcements.Insert(player2, "LST", passengers, { lcfentry3.Location, lcfdropoff3.Location  }, { lcfentry3.Location })
-end
 
 
 
 
 
 SendTSAVehicles = function()
-	Reinforcements.Reinforce(player3, TSAV, TSAentry1.Location, TSARally1.Location, Utils.Seconds(1), function(actor)
+	Reinforcements.Reinforce(player3, TEAM3V, TSAentry1.Location, TSARally1.Location, Utils.Seconds(1), function(actor)
 		Actor.SetStance(actor, "Defend")
 	end)
 end
 
 
 SendInsertionHelicopter = function()
-	local heli, passengers = Reinforcements.Insert(player3, "tran", TanyaType,
+	local heli, passengers = Reinforcements.Insert(player3, "tran", TEAM3INF1,
 		{ TSAEntry2.Location, TSARally2.Location }, { HeliExtract.Location })
-	local heli, passengers = Reinforcements.Insert(player3, "tran", TanyaType,
+	local heli, passengers = Reinforcements.Insert(player3, "tran", TEAM3INF1,
 		{ TSAEntry2.Location, TSARally3.Location }, { HeliExtract.Location })
 	SendInsertionHelicopter2()
 end
 
 SendInsertionHelicopter2 = function()
-	local heli, passengers = Reinforcements.Insert(player3, "tran", TanyaType,
+	local heli, passengers = Reinforcements.Insert(player3, "tran", TEAM3INF1,
 		{ TSAEntry2.Location, TSARally2.Location }, { HeliExtract.Location })
-	local heli, passengers = Reinforcements.Insert(player3, "tran", Rockets,
+	local heli, passengers = Reinforcements.Insert(player3, "tran", TEAM3INF2,
 		{ TSAEntry2.Location, TSARally3.Location }, { HeliExtract.Location })
 
 end
